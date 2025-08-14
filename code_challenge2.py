94c9a808053986a9d2ae61a9fe33f0439caf4217
cristinejaen <christinejaen4@gmail.com>
2025-08-14 11:07:49 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,18 @@
+instagram= eval(input("enter account to sitting --> "))
+print ("this is the breakdown of the beautiful picture :")
+print("1000 : ", instagram // 1000 )
+instagram %= 1000
+print("500 : ", instagram // 500 )
+instagram %= 500
+print("200 : ", instagram // 200 )
+instagram %= 200
+print("100 : ", instagram // 100 )
+instagram %= 100
+print("50 : ", instagram // 50 )
+instagram %= 50
+print("20 : ", instagram // 20 )
+instagram %= 20
+print("5 : ", instagram // 5 )
+instagram %= 5
+print("1 : ", instagram // 1 )
+instagram %= 1
