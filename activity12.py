495b648e7dca17e6444272204a355664b8b49fdc
Christine Jaen <christinejaen4@gmail.com>
2025-09-16 20:40:42 +0800

    Add files via upload

---
+++ activity12.py
@@ -0,0 +1,7 @@
+#print hello world 10 times
+
+for cycle in range(1,10,1):
+    print(cycle, "Hello World")
+# #print hello world 10 times
+# for cycle in range(1,11,1):
+#     print(cycle, "Hello World")
---
+++ activity13.py
@@ -0,0 +1,7 @@
+
+for x in range(1, 11,1):
+    sum = 0
+    print(x)
+    number = eval(input("Enter any number -->"))
+    sum =+ number
+    print("\n\tThe sum of all the number give is ", sum)
