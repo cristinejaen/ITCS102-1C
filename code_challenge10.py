71c387717dd771ce1ee31b144db990e8f0b1ba4b
Christine Jaen <christinejaen4@gmail.com>
2025-09-25 20:46:03 +0800

    Add files via upload

---
+++ code_challenge10.py
@@ -0,0 +1,9 @@
+
+for loop in range(10,0,-1):
+    for x in range(1,loop):
+        print("*", end=" ")
+    print()
+for loop in range(1,11):
+    for x in range(1,loop):
+        print("*", end=" ")
+    print()
