25c6c55bb1a2a0944cc1c540e82ee9d95cb08c22
Christine Jaen <christinejaen4@gmail.com>
2025-11-14 01:08:47 +0800

    Delete activity23.py

--- activity23.py
+++
@@ -1,9 +0,0 @@
-# Define a function named MyFirstFunction
-def MyFirstFunction ():
-    print("Hello, this is my first function!")
-# Call the function
-    print("Function execution completed.")
-    print("Thank you!")
-
-
-MyFirstFunction()
