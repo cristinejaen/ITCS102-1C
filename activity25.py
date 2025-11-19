9ad7252dd4b66145a4f0253fb4e62c8a3f7f699b
Christine Jaen <christinejaen4@gmail.com>
2025-11-19 21:11:21 +0800

    Add files via upload

---
+++ activity25.py
@@ -0,0 +1,10 @@
+#LIST
+# animal languages
+animal_languages = ['cat','pet','dog','fish','bird']
+print("Animal Language Translator"[8])
+
+# dictornary
+empty_dictionary= {}
+
+pl_dictionary = { 'example_word' }
+print(pl_dictionary)
---
+++ code_challenge15.py
@@ -0,0 +1,36 @@
+import os
+print('DLL STUDENT INFORMATION SYSTEM')
+print('===============================')
+
+while True:
+    print("SELECT FROM THE FOLLOWING OPTIONS:")
+    print("A. adding student record")
+    print("B. sesarch student record")
+    print("C. edit student record")
+    print("D. delete student record")
+    print("E. print all record")
+    print("F. export data")
+    print("G. exit system")
+
+    elite = input("ENTER HERE: ").lower()
+    if elite == 'a':
+        student_id = input("Enter Student ID: ")
+        continue
+    elif elite == 'b':
+        pass
+        continue
+    elif elite == 'c':
+        pass
+        continue
+    elif elite == 'd':
+        pass
+        continue
+    elif elite == 'e':
+        pass
+        continue
+    elif elite == 'f':
+        pass
+        continue
+    elif elite == 'g':
+        print("Exiting the system. Goodbye!")
+        break
